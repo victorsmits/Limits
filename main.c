276ea6cc7dd0c4ff3df35c1b6d450338c6cdfcea
victorsmits <victor.smits@student.vinci.be>
2018-10-30 15:17:06 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <Limits.h>
+
+int main() {
+    printf("=== long int ===");
+    printf("\nBITS :  %d", LONG_BIT);
+    printf("\nMIN : %li", LONG_MIN);
+    printf("\nMAX : %li", LONG_MAX );
+}
